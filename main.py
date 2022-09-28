e58a5107e7e65aa65b0daed3ff2d9a373c77f751
Martin Ansteensen <maana001@osloskolen.no>
2022-09-28 11:04:12 +0200

    Added attack and get_attacked method to creature class. Created Hero and Monster class

--- main.py
+++ main.py
@@ -5,4 +5,17 @@ class Creature:
         self.hp=hp
         self.attackPoints=attackPoints
 
-    def 
+    def attack(self, object_to_attack):
+        object_to_attack.get_attacked(self.attack_points)
+    
+    def get_attacked(self, hp_reduce):
+        self.hp -= hp_reduce
+
+class Hero(Creature):
+    def __init__(self, name, hp, attack_points):
+        super.__init__(name, hp, attack_points)
+    
+
+class Monster(Creature):
+    def __init__(self, name, hp, attack_points):
+        super.__init__(name, hp, attack_points)
