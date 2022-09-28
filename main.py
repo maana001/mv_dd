3edbfdae25822555c46c7b0fc8a8ba3f8b97a501
vewia002 <vewia002@osloskolen.no>
2022-09-28 11:04:16 +0200

    Added giveInfo and checkHealth methods to Creature

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 
 class Creature:
-    def __init__(self,name, hp,attackPoints):
+    def __init__(self,name, hp,attack_points):
         self.name=name
         self.hp=hp
-        self.attackPoints=attackPoints
+        self.attack_points=attack_points
 
-    def 
+    def giveInfo(self):
+        print("###################")
+        print("This is my life story")
+        print("hp: ", self.hp)
+        print("name: ", self.name)
+        print("attackPoints:", self.attack_points)
+        print("###################")
+
+    def checkHealth(self):
+        if self.hp==0:
+            print()
