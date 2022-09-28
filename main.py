3d07283a9e5ac41ec072bdda9a13bc0c585fcf90
vewia002 <vewia002@osloskolen.no>
2022-09-28 11:27:36 +0200

    Added NorskTeacher class

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 
+import random as rd
+
 class Creature:
     def __init__(self,name, hp,attack_points):
         self.name=name
@@ -18,6 +20,14 @@ class Creature:
     
     def get_attacked(self, hp_reduce):
         self.hp -= hp_reduce
+        self.checkHealth()
+
+    def checkHealth(self):
+        if self.hp==0:
+            print(f"{self.name} of class{a.__class__.__name__ } is dead" )
+        else: 
+            print(f"{self.name} of class{a.__class__.__name__ } has {self.hp} hp left")
+            
 
 class Hero(Creature):
     def __init__(self, name, hp, attack_points):
@@ -26,6 +36,17 @@ class Hero(Creature):
 
 class Monster(Creature):
     def __init__(self, name, hp, attack_points):
-        super.__init__(name, hp, attack_points)checkHealth(self):
-        if self.hp==0:
-            print()
+        super.__init__(name, hp, attack_points)
+
+
+class NorskTeacher(Monster):
+    def __init__(self,name, hp, attack_points):
+        super.__init__(name, hp, attack_points)
+
+    def supriseTest(self):
+        """
+        The teacher has decided to test the class with a suprise test!
+        50% chance of double damage if nynorsk test!
+        """
+        nynorsk_test=rd.randint(0,1)
+        
