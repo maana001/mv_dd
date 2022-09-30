c19f33151b047b7354993c5de76afbb8af0bb4be
Martin Ansteensen <maana001@osloskolen.no>
2022-09-30 14:18:43 +0200

    First working version with gameplay

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ class Hero(Creature):
         super().__init__(name, hp, attack_points)
 
     def heal(self):
-        self.hp += 20
+        self.hp += 100
         if self.hp > 100:
             self.hp = 100
         self.checkHealth()
@@ -48,62 +48,60 @@ class Monster(Creature):
         self.target = target
 
     def should_attack(self):
-        if rd.randint(1,2) == 1:
-            print("Monster is attacking")
+        if rd.randint(1,2) == 1 and self.is_dead == False:
+            print("%s monster is attacking" %(self.name))
             self.attack(self.target)
         else:
-            print("Monster is not attacking")
+            print("%s monster is not attacking" %(self.name))
 
     def attack(self, object_to_attack):
-        if self.is_dead==False:
-           object_to_attack.get_attacked(self.attack_points)
+        object_to_attack.get_attacked(self.attack_points)
 
 class NorskTeacher(Monster):
     def __init__(self,name, hp, attack_points, target):
         super().__init__(name, hp, attack_points, target)
 
-    def supriseTest(self):
+    def supriseTest(self, object_to_attack):
         """
         The teacher has decided to test the class with a suprise test!
         50% chance of double damage if nynorsk test!
         """
         nynorsk_test=rd.randint(0,1)
         if nynorsk_test==1:
-            self.attack_points=self.attack_points*2
-    
+            object_to_attack.get_attacked(self.attack_points*2)    
 
-    def spanishTube(self):
+    def spanishTube(self, object_to_attack):
         """
         You have been naughty
         Time for punishment! The teacher deals 1.2 times damage!
         """  
-        self.attack_points=self.attack_points*1.2
+        object_to_attack.get_attacked(self.attack_points*1.2)
     
-    def nyNorskExam(self):
+    def nyNorskExam(self, object_to_attack):
         """
         You have your nynorsk exam tomorrow.
         This means instant death
         """
-        self.attack_points=1000
+        object_to_attack.get_attacked(self.attack_points*200)
 
     def attack(self, object_to_attack):
         attack_method=rd.randint(1,100)
+        
         if attack_method<=10:
-            self.supriseTest()
+            self.supriseTest(object_to_attack)
+        
         if attack_method<40 and attack_method>10:
-            self.spanishTube()
-        if attack_method==100:
-            self.nyNorskExam()
-            
-        if self.is_dead==False:
-           object_to_attack.get_attacked(self.attack_points)
+            self.spanishTube(object_to_attack)
 
+        if attack_method==100:
+            self.nyNorskExam(object_to_attack)
+            
 
 if __name__== "__main__":
     hero_player = Hero("Knud Knudsen", 100, 20)
 
-    ivar_monster = Monster("Ivar Aasen", 100, 50, hero_player)
-    ingvild_teacher = NorskTeacher("Ingvild", 100, 10, hero_player)
+    ivar_monster = Monster("Ivar Aasen", 10, 50, hero_player)
+    ingvild_teacher = NorskTeacher("Ingvild", 10, 10, hero_player)
     monster_list = [ivar_monster, ingvild_teacher]
     while True:
         print("\n")
@@ -112,13 +110,22 @@ if __name__== "__main__":
             print("Game over")
             exit()
         
+        # Check all monsters are dead
+        if not monster_list:
+            print("You won!")
+            exit()
+
         # Players turn
         while True:
             choice = input("Enter a for attack and h for heal: ")
             if choice == "a":
-                print("Player is attacking")
-                hero_player.attack(monster_list[0])
+                # Choose random monster to attack
+                monster_to_attack = rd.choice(monster_list)
+                print("Player is attacking %s " %(monster_to_attack.name))
+                # Attack monster
+                hero_player.attack(monster_to_attack)
                 break
+
             elif choice == "h":
                 print("Player is healing")
                 hero_player.heal()
